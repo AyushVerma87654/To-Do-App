153ce0cbbf65b6dba0f3ad3e2eb292ccf4fe0ee1
Ayush Verma <tgmcodeyogi@gmail.com>
2022-10-07 13:42:55 +0530

    Everything Working Now

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,47 +4,48 @@ import Display from "./Display";
 import { ImCross } from "react-icons/im";
 
 function Todo() {
-  // const todoStorage = localStorage.getItem("Todo") || "{}";
-  // const doneStorage = localStorage.getItem("Done") || "{}";
-  // const td = JSON.stringify(todoStorage);
-  // const dd = JSON.stringify(doneStorage);
-  // console.log(td);
-  // console.log(dd);
-  // const [todo, setTodo] = useState(td);
-  // const [done, setDone] = useState(dd);
-
-  const td = { "Bring Milk and Bread": "Bring Milk and Bread" };
-  const dd = { "Clean my room": "Clean my room" };
+  const todoStorage = localStorage.getItem("Todo") || "[]";
+  const doneStorage = localStorage.getItem("Done") || "[]";
+  const td = JSON.parse(todoStorage);
+  const dd = JSON.parse(doneStorage);
   const [todo, setTodo] = useState(td);
   const [done, setDone] = useState(dd);
 
   const onAddFromTodo = (data) => {
     updatedDoneObject(data);
-    const newTodo = { ...todo };
-    delete newTodo[data];
-    updateTodo(newTodo);
+    const newTodo = [...todo];
+    const newTodoo = newTodo.filter((item) => {
+      if (item != data) {
+        return item;
+      }
+    });
+    updateTodo(newTodoo);
   };
 
-  const updatedTodoObject = (data) => updateTodo({ ...todo, [data]: data });
+  const updatedTodoObject = (data) => updateTodo([...todo, data]);
 
   const updateTodo = (data) => {
-    setTodo({ ...data });
+    setTodo(data);
     const todoString = JSON.stringify(data);
     localStorage.setItem("Todo", todoString);
   };
 
   const addTodo = (data) => {
-    const newTodo = { ...todo, [data]: data };
-    setTodo(newTodo);
+    const newTodo = [...todo, data];
+    updateTodo(newTodo);
   };
 
-  const updatedDoneObject = (data) => updateDone({ ...done, [data]: data });
+  const updatedDoneObject = (data) => updateDone([...done, data]);
 
   const onAddFromDone = (data) => {
     updatedTodoObject(data);
-    const newDone = { ...done };
-    delete newDone[data];
-    updateDone(newDone);
+    const newDone = [...done];
+    const newDonee = newDone.filter((item) => {
+      if (item != data) {
+        return item;
+      }
+    });
+    updateDone(newDonee);
   };
 
   const updateDone = (data) => {
@@ -56,10 +57,13 @@ function Todo() {
   const handleRemoveClick = (data) => removeDone(data);
 
   const removeDone = (data) => {
-    console.log(data);
-    const newDone = { ...done };
-    delete newDone[data];
-    updateDone(newDone);
+    const newDone = [...done];
+    const newDonee = newDone.filter((item) => {
+      if (item != data) {
+        return item;
+      }
+    });
+    updateDone(newDonee);
   };
 
   return (
@@ -67,28 +71,29 @@ function Todo() {
       <h1 className="text-blue-700 font-bold mb-10 text-3xl">My Todo App</h1>
       <h2 className="my-4">Things to be done</h2>
       <div className="my-4">
-        {Object.keys(todo).map((item) => (
-          <Display key={item} data={item} onAdd={onAddFromTodo} />
-        ))}
+        {todo &&
+          todo.map((item) => (
+            <Display key={item} data={item} onAdd={onAddFromTodo} />
+          ))}
       </div>
       <AddTodo addTodo={addTodo} />
       <h2 className="my-4">Things done</h2>
       <div className="my-4">
-        {Object.keys(done).map((item) => (
-          <div className="flex space-x-5 item">
-            <Display
-              key={item}
-              data={item}
-              onAdd={onAddFromDone}
-              checked="true"
-              readOnly="readOnly"
-            />
-            <ImCross
-              className="w-2 text-end"
-              onClick={(event) => handleRemoveClick(item)}
-            />
-          </div>
-        ))}
+        {done &&
+          done.map((item) => (
+            <div className="flex space-x-5 item">
+              <Display
+                key={item}
+                data={item}
+                onAdd={onAddFromDone}
+                checked="true"
+              />
+              <ImCross
+                className="w-2 text-end"
+                onClick={(event) => handleRemoveClick(item)}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
