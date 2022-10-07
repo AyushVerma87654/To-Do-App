cb3bda66871b5c650fc72a7bbf7e36d1ab4f9edd
Ayush Verma <tgmcodeyogi@gmail.com>
2022-10-07 10:00:45 +0530

    Updated without retriving data from Local Storage due to errors

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Display({ data, onAdd, checked }) {
+function Display({ data, onAdd, checked, readOnly }) {
   return (
     <div className="flex h-4 space-x-2 items-center">
       <input
+        readOnly={readOnly}
         checked={checked}
         className="-mb-1"
         type="checkbox"
